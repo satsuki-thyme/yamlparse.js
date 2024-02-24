c0e419ed53d5034140ed9127428ca0e8038fa727
satsuki-thyme <satsuki.thyme@gmail.com>
2024-02-25 04:49:02 +0900

    comment, blankを除去して一応の安定版

--- yamlparse.js
+++ yamlparse.js
@@ -2,204 +2,298 @@ function yamlparse(rawYaml) {
   let splYaml = rawYaml.split(/\r?\n/)
   let procYaml = []
   let meaning = []
+  let EOA = splYaml.length - 1
   for (let i in splYaml) {
-    if (/^[ \t]*(?<!\\)#.*$/.test(splYaml[i])) {
+    if (/^[ \t]*(?<!\\(\\\\)*)#.*$/.test(splYaml[i])) {
       meaning[i] = `comment`
+    }
+    else if (/^((?!.*\\(\\\\)*:).*?)(?<!\\(\\\\)*)(?=:)/.test(splYaml[i])) {
+      meaning[i] = `key`
+    }
+    else if (/^[ \t]*(?!.*(\\\\)*#).+$/.test(splYaml[i])) {
+      meaning[i] = `value`
     }
     else if (/^[ \t]*$/.test(splYaml[i])) {
       meaning[i] = `blank`
     }
     else {
-      meaning[i] = `effect`
+      meaning[i] = `other`
     }
   }
+  let modeSet = new Set()
   let indentVal = []
   for (let i in splYaml) {
-    if (meaning[i] === `effect`) {
-      indentVal[i] = (splYaml[i].match(/^[ \t]*/) || [])[0].length
+    let w = (splYaml[i].match(/^[ \t]*/) || [])[0].length
+    splYaml[i] = splYaml[i].replace(/^[ \t]*/, ``)
+    if (meaning[i] === `key`) {
+      modeSet.add(w)
     }
-    else {
-      indentVal[i] = 0
-    }
+    indentVal.push(w)
   }
-  let indentDiff = {}
-  for (let i = 1; i < indentVal.length; i++) {
-    if (indentVal[i - 1] < indentVal[i]) {
-      if (indentDiff[indentVal[i] - indentVal[i - 1]]) {
-        indentDiff[indentVal[i] - indentVal[i - 1]]++
-      }
-      else {
-        indentDiff[indentVal[i]] = 1
-      }
-    }
-  }
-  let mode = Number(Object.keys(indentDiff)[0])
-  for (let i = 1; i < Object.keys(indentDiff).length; i++) {
-    if (indentDiff[Object.keys(indentDiff)[i]] > indentDiff[Object.keys(indentDiff)[i - 1]]) {
-      mode = Number(Object.keys(indentDiff)[i])
-    }
-  }
+  let mode = Array.from(new Set(Array.from(modeSet).reduce((a, b) => [b, a[1].concat([b - a[0]])], [0, [0]])[1]))
+  .filter(rly => rly !== 0)[0]
   let indentNum = []
   for (let i in indentVal) {
-    if (meaning[i] === `effect`) {
-      indentNum[i] = Math.round(indentVal[i] / mode)
-    }
-    else {
-      indentNum[i] = 0
-    }
+    indentNum[i] = Math.round(indentVal[i] / mode) + 1
   }
-  let prevEffectIndentNum = 0
-  for (let i = 0; i < indentNum.length; i++) {
-    /*
-    #######    #######    #######    #######     ######    ######## 
-    ##         ##         ##         ##         ##            ##    
-    #####      #####      #####      #####      ##            ##    
-    ##         ##         ##         ##         ##            ##    
-    #######    ##         ##         #######     ######       ##    
-    */
-    if (meaning[i] === `effect`) {
-      let diff = indentNum[i] - prevEffectIndentNum
-      prevEffectIndentNum = indentNum[i]
-      if (i > 0 && i < splYaml.length - 1) {
-        if (diff > 0) {
-          procYaml[i] = `${`\n<div>`.repeat(diff)}\n${splYaml[i].replace(/^[ \t]*/m, ``)}`
-        }
-        if (diff === 0) {
-          procYaml[i] = `\n<br>\n${splYaml[i].replace(/^[ \t]*/, ``)}`
-        }
-        if (diff < 0) {
-          procYaml[i] = `${`\n</div>`.repeat(-diff)}\n${splYaml[i].replace(/^[ \t]*/, ``)}`
-        }
-      }
-      if (i === 0 && i < splYaml.length - 1) {
-        procYaml[i] = `${`\n<div>`.repeat(indentNum[i] + 1)}\n${splYaml[i].replace(/^[ \t]*/, ``)}`
-      }
-      if (i > 0 && i === splYaml.length - 1) {
-        procYaml[i] = `${`\n</div>`.repeat(indentNum[i] + 1)}\n${splYaml[i].replace(/^[ \t]*/, ``)}\n</div>`
-      }
-      if (splYaml.length === 1) {
-        procYaml[i] = splYaml[i].replace(/^[ \t]*/, ``)
-      }
-    }
-    /*
-    ######     ##          #####     ###    ##    ##   ## 
-    ##   ##    ##         ##   ##    ####   ##    ##  ##  
-    ######     ##         #######    ## ##  ##    #####   
-    ##   ##    ##         ##   ##    ##  ## ##    ##  ##  
-    ######     #######    ##   ##    ##   ####    ##   ## 
-    */
-    if (meaning[i] === `blank`) {
-      if (i > 0 && i < splYaml.length - 1) {
-        if (meaning[i - 1] === `blank` && meaning[i + 1] === `blank`) {
-          procYaml[i] = `\n<br>`
-        }
-        if (meaning[i - 1] !== `blank` && meaning[i + 1] === `blank`) {
-          procYaml[i] = `\n<div>\n<br>`
-        }
-        if (meaning[i - 1] === `blank` && meaning[i + 1] !== `blank`) {
-          procYaml[i] = `\n<br>\n</div>`
-        }
-        if (meaning[i - 1] !== `blank` && meaning[i + 1] !== `blank`) {
-          procYaml[i] = `\n<div>\n<br>\n</div>`
-        }
-      }
-      if (i === 0 && i < splYaml.length - 1) {
-        if (meaning[i + 1] === `blank`) {
-          procYaml[i] = `\n<div>\n<br>`
-        }
-        if (meaning[i + 1] !== `blank`) {
-          procYaml[i] = `\n<div>\n<br>\n</div>`
-        }
-      }
-      if (i > 0 && i === splYaml.length - 1) {
-        if (meaning[i - 1] === `blank`) {
-          procYaml[i] = `\n<br>\n</div>`
-        }
-        if (meaning[i - 1] !== `blank`) {
-          procYaml[i] = `\n<div>\n<br>`
-        }
-      }
-      if (splYaml.length === 1) {
-        procYaml[i] = `<div>\n<br>\n</div>`
-      }
-    }
-    /*
-     ######     ######     ###    ###    ###    ###    #######    ###    ##    ######## 
-    ##         ##    ##    ####  ####    ####  ####    ##         ####   ##       ##    
-    ##         ##    ##    ## #### ##    ## #### ##    #####      ## ##  ##       ##    
-    ##         ##    ##    ##  ##  ##    ##  ##  ##    ##         ##  ## ##       ##    
-     ######     ######     ##      ##    ##      ##    #######    ##   ####       ##    
-    */
-    if (meaning[i] === `comment`) {
-      if (i > 0 && i < splYaml.length - 1) {
-        if (meaning[i - 1] === `comment` && meaning[i + 1] === `comment`) {
-          procYaml[i] = `\n${splYaml[i]}`
-        }
-        if (meaning[i - 1] !== `comment` && meaning[i + 1] === `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1`)
-        }
-        if (meaning[i - 1] === `comment` && meaning[i + 1] !== `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n$1</pre>`)
-        }
-        if (meaning[i - 1] !== `comment` && meaning[i + 1] !== `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1</pre>`)
-        }
-      }
-      if (i === 0 && i < splYaml.length - 1) {
-        if (meaning[i + 1] === `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1`)
-        }
-        if (meaning[i + 1] !== `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1</pre>`)
-        }
-      }
-      if (i > 0 && i === splYaml.length - 1) {
-        if (meaning[i - 1] === `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n$1</pre>`)
-        }
-        if (meaning[i - 1] !== `comment`) {
-          procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1</pre>`)
-        }
-      }
-      if (splYaml.length === 1) {
-        procYaml[i] = splYaml[i].replace(/^([ \t]*(?<!\\)#.*)/, `\n<pre>$1</pre>`)
-      }
-    }
-    /*
-    ###    ##     ######     ########        #######    #######    #######    #######     ######    ######## 
-    ####   ##    ##    ##       ##           ##         ##         ##         ##         ##            ##    
-    ## ##  ##    ##    ##       ##           #####      #####      #####      #####      ##            ##    
-    ##  ## ##    ##    ##       ##           ##         ##         ##         ##         ##            ##    
-    ##   ####     ######        ##           #######    ##         ##         #######     ######       ##    
-    */
-    if (meaning[i] !== `effect`) {
-      if (i > 0 && i < splYaml.length - 1) {
-        if (meaning[i - 1] === `effect` && meaning[i + 1] !== `effect`) {
-          procYaml[i] = `${`\n</div>`.repeat(prevEffectIndentNum)}${procYaml[i]}`
-        }
-        if (meaning[i - 1] !== `effect` && meaning[i + 1] === `effect`) {
-          procYaml[i] = `${procYaml[i]}${`\n<div>`.repeat(prevEffectIndentNum)}`
-        }
-        if (meaning[i - 1] === `effect` && meaning[i + 1] === `effect`) {
-          procYaml[i] = `${`\n</div>`.repeat(prevEffectIndentNum)}${procYaml[i]}${`\n<div>`.repeat(prevEffectIndentNum)}`
-        }
-      }
-      if (i > 0 && i === splYaml.length - 1) {
-        if (i > 0 && i < splYaml.length - 1) {
-          if (meaning[i - 1] === `effect`) {
-            procYaml[i] = `${`\n</div>`.repeat(prevEffectIndentNum)}${procYaml[i]}`
+  let prevIndentNum = 0
+  let keyAcc = []
+  let classAcc = []
+  let keyAggSrc = new Set()
+  let i = 0
+  return new Promise(resolve => {
+    fn()
+    function fn() {
+      /*
+      ##   ##    #######    ##    ## 
+      ##  ##     ##          ##  ##  
+      #####      #####        ####   
+      ##  ##     ##            ##    
+      ##   ##    #######       ##    
+      */
+      if (meaning[i] === `key`) {
+        let diff = indentNum[i] - prevIndentNum
+        prevIndentNum = indentNum[i]
+        let key = `${
+          (
+            splYaml[i]
+            .match(/^((?!.*\\(\\\\)*:).*)(?<!\\(\\\\)*)(?=:)/) || [``]
+          )[0]
+          .replace(/^(- )?(.*)/, `$1<span class="key">$2</span>`)
+          .replace(/^- /g, `<span class="bullet">-</span> `)
+        }<span class="colon">:</span> `
+        let clss = `key-group ${
+          (
+            splYaml[i]
+            .match(/^((?!.*\\(\\\\)*:).*)(?<!\\(\\\\)*)(?=:)/) || [``]
+          )[0]
+          .replace(/ /g, `-`)
+          .replace(/^([0-9])/, `n$1`)
+        }`
+        let keySrc = (splYaml[i].match(/^((?!.*\\(\\\\)*:).*)(?<!\\(\\\\)*)(?=:)/) || [``])[0].replace(/^- /, ``)
+        keyAggSrc.add(keySrc)
+        let value = `${
+          (
+            splYaml[i]
+            .match(/(?<=:)(.*)$/) || [``]
+          )[0]
+          .replace(/(.+)/, `<span class="value">$1</span>`)
+        }`
+        if (i > 0 && i < EOA) {
+          if (diff > 0) {
+            procYaml[i] = `<div class="${clss}">${key}${value}`
+            keyAcc.push(keySrc)
+            classAcc.push(clss)
+          }
+          if (diff === 0) {
+            let frontEnd = `</div>`
+//            if (meaning[i - 1] === `blank` || meaning[i - 1] === `comment`) {
+//              frontEnd = ``
+//            }
+            procYaml[i] = `${frontEnd}<div class="${clss}">${key}${value}`
+            keyAcc.splice(-1)
+            classAcc.splice(-1)
+            keyAcc.push(keySrc)
+            classAcc.push(clss)
+          }
+          if (diff < 0) {
+            let frontEnd = `</div>`.repeat(-diff + 1)
+//            if (meaning[i - 1] === `blank` || meaning[i - 1] === `comment`) {
+//              frontEnd = ``
+//            }
+            procYaml[i] = `${frontEnd}<div class="${clss}">${key}${value}`
+            keyAcc.splice(diff)
+            classAcc.splice(diff)
+            keyAcc.push(keySrc)
+            classAcc.push(clss)
           }
         }
+        if (i === 0 && i < EOA) {
+          procYaml[i] = `<div class="${clss}">${key}${value}`
+          keyAcc.push(keySrc)
+          classAcc.push(clss)
+        }
+        if (i > 0 && i === EOA) {
+          procYaml[i] = `${`</div>`.repeat(-diff + 1)}<div class="${clss}">${key}${value}${`</div>`.repeat(indentNum[i] - 1)}`
+          keyAcc.splice(diff)
+          classAcc.splice(diff)
+          keyAcc.push(keySrc)
+          classAcc.push(clss)
+        }
+        if (EOA === 0) {
+          procYaml[i] = `<div class="${clss}">${key}${value}${`</div>`.repeat(indentNum[i] - 1)}`
+          keyAcc.push(keySrc).splice(-indentNum[i] + 1)
+          classAcc.push(clss).splice(-indentNum[i] + 1)
+        }
+      }
+      /*
+      ##    ##     #####     ##         ##    ##    ####### 
+      ##    ##    ##   ##    ##         ##    ##    ##      
+      ##    ##    #######    ##         ##    ##    #####   
+       ##  ##     ##   ##    ##         ##    ##    ##      
+        ####      ##   ##    #######     ######     ####### 
+      */
+      if (meaning[i] === `value`) {
+        let diff = indentNum[i] - prevIndentNum
+        prevIndentNum = indentNum[i]
+        let value = `${
+          splYaml[i]
+          .replace(/^(- )?(.*)$/, `$1<span class="value">$2</span>`)
+          .replace(/^- /, `<span class="bullet">-</span> `)
+        }`
+        if (i > 0 && i < EOA) {
+          if (diff > 0) {
+            procYaml[i] = `<div class="value-group">${value}`
+          }
+          if (diff === 0) {
+            procYaml[i] = `<br>${value}`
+          }
+          if (diff < 0) {
+            procYaml[i] = `${`</div>`.repeat(-diff)}${value}`
+          }
+        }
+        if (i === 0 && i < EOA) {
+          if (meaning[i + 1] === `value`) {
+            procYaml[i] = `<div>${value}`
+          }
+          if (meaning[i + 1] !== `value`) {
+            procYaml[i] = `<div>${value}</div>`
+          }
+        }
+        if (i > 0 && i === EOA) {
+          if (meaning[i - 1] === `value`) {
+            procYaml[i] = `<br>${value}${`</div>`.repeat(indentNum[i] - 1)}`
+          }
+          if (meaning[i - 1] !== `value`) {
+            procYaml[i] = `<div>${value}${`</div>`.repeat(indentNum[i] - 1)}`
+          }
+        }
+        if (EOA === 0) {
+          procYaml[i] = `<div>${value}${`</div>`.repeat(indentNum[i] - 1)}`
+        }
+      }
+      /*
+       ######     ######     ###    ###    ###    ###    #######    ###    ##    ######## 
+      ##         ##    ##    ####  ####    ####  ####    ##         ####   ##       ##    
+      ##         ##    ##    ## #### ##    ## #### ##    #####      ## ##  ##       ##    
+      ##         ##    ##    ##  ##  ##    ##  ##  ##    ##         ##  ## ##       ##    
+       ######     ######     ##      ##    ##      ##    #######    ##   ####       ##    
+      */
+      if (meaning[i] === `comment`) {
+        if (i > 0 && i < EOA) {
+          if (meaning[i - 1] === `comment` && meaning[i + 1] === `comment`) {
+            procYaml[i] = ``
+          }
+          if (meaning[i - 1] !== `comment` && meaning[i + 1] === `comment`) {
+            procYaml[i] = ``
+          }
+          if (meaning[i - 1] === `comment` && meaning[i + 1] !== `comment`) {
+            procYaml[i] = ``
+          }
+          if (meaning[i - 1] !== `comment` && meaning[i + 1] !== `comment`) {
+            procYaml[i] = ``
+          }
+        }
+        if (i === 0 && i < EOA) {
+          if (meaning[i + 1] === `comment`) {
+            procYaml[i] = ``
+          }
+          if (meaning[i + 1] !== `comment`) {
+            procYaml[i] = ``
+          }
+        }
+        if (i > 0 && i === EOA) {
+          if (meaning[i - 1] === `comment`) {
+            procYaml[i] = ``
+          }
+          if (meaning[i - 1] !== `comment`) {
+            procYaml[i] = ``
+          }
+        }
+        if (EOA === 0) {
+          procYaml[i] = ``
+        }
+      }
+      /*
+      ######     ##          #####     ###    ##    ##   ## 
+      ##   ##    ##         ##   ##    ####   ##    ##  ##  
+      ######     ##         #######    ## ##  ##    #####   
+      ##   ##    ##         ##   ##    ##  ## ##    ##  ##  
+      ######     #######    ##   ##    ##   ####    ##   ## 
+      */
+      if (meaning[i] === `blank`) {
+        if (i > 0 && i < EOA) {
+          if (meaning[i - 1] === `blank` && meaning[i + 1] === `blank`) {
+            procYaml[i] = `<br>`
+          }
+          if (meaning[i - 1] !== `blank` && meaning[i + 1] === `blank`) {
+            procYaml[i] = `<br>`
+          }
+          if (meaning[i - 1] === `blank` && meaning[i + 1] !== `blank`) {
+            procYaml[i] = `<br>`
+          }
+          if (meaning[i - 1] !== `blank` && meaning[i + 1] !== `blank`) {
+            procYaml[i] = `<br>`
+          }
+        }
+        if (i === 0 && i < EOA) {
+          if (meaning[i + 1] === `blank`) {
+            procYaml[i] = `<br>`
+          }
+          if (meaning[i + 1] !== `blank`) {
+            procYaml[i] = `<br>`
+          }
+        }
+        if (i > 0 && i === EOA) {
+          if (meaning[i - 1] === `blank`) {
+            procYaml[i] = `<br>`
+          }
+          if (meaning[i - 1] !== `blank`) {
+            procYaml[i] = `<br>`
+          }
+        }
+        if (EOA === 0) {
+          procYaml[i] = `<br>`
+        }
+      }
+      /*
+       ######     ########    ##   ##    #######    ######  
+      ##    ##       ##       ##   ##    ##         ##   ## 
+      ##    ##       ##       #######    #####      ######  
+      ##    ##       ##       ##   ##    ##         ##   ## 
+       ######        ##       ##   ##    #######    ##   ## 
+      */
+       if (meaning[i] === `other`) {
+        if (i > 0 && i < EOA) {
+        }
+        if (i === 0 && i < EOA) {
+        }
+        if (i > 0 && i === EOA) {
+        }
+        if (EOA === 0) {
+        }
+      }
+      if (i < EOA) {
+        i++
+        fn()
+      }
+      else {
+        resolve(procYaml)
       }
     }
+  })
+  .then(rly => {
+    return [rly.join(``), procKey()]
+  })
+  function procKey() {
+    let keyAgg = Array.from(keyAggSrc)
+    .map(rly => {
+      let w = {}
+      w[rly] = rly
+      .replace(/ /g, `-`)
+      .replace(/^([0-9])/, `n$1`)
+      return w
+    })
+    
   }
-  return procYaml
-  .join(``)
-  .replace(/(#.*)/g, `<span class="comment">$1</span>`)
-  .replace(/(?<=\n|^)(- )?(\d+):/g, `$1<span class="number">$2</span>:`)
-  .replace(/(?<=\n|^)((?!.*\\(\\\\)*:).*?)(?<!\\(\\\\)*)(?=:)/g, `<span class="key">$1</span>`)
-  .replace(/(?<=\n|^)((?!.*\\(?:\\\\)*:).*?)(?<!\\(?:\\\\)*): [ \t]*(.+)(\n|$)/g, `$1:<span class="value">$2</span>$3`)
-  .replace(/(?<=\n|^)((?!.*\\(?:\\\\)*:).*?)(?<!\\(?:\\\\)*):/g, `$1<span class="colon">:</span>`)
-  .replace(/(?<=\n|^)- /g, `<span class="bullet">-</span>`)
-  .replace(/(?<=\n|^)\|/g, `<span class="vertical-bar">|</span>`)
 }
